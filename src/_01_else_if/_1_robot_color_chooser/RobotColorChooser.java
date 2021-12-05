62ab202f2361842524982153f1283568fa55d3e1
student <student@ip-172-31-61-241.us-west-2.compute.internal>
2021-12-04 16:25:56 -0800

    RAINBOW CRAZY LINES!

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -1,4 +1,3 @@
-
 package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
@@ -10,30 +9,128 @@ import java.awt.Color;
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		Robot robo = new Robot();
-		int i = 0;
 		robo.penDown();
-		robo.setSpeed(100);
+		robo.setSpeed(200);
 		
 		while(true) {
-			robo.setPenWidth(10);
+			robo.setPenWidth(5);
 			
-			String n = JOptionPane.showInputDialog("What shape should Robo draw this time?\n" + "[PLEASE ENTER THE NUMBER OF SIDES]");
+			int rainbow = 0;
+			int crazy = 0;
+			
+			String nS = JOptionPane.showInputDialog("What shape should Robo draw this time?\n" + "[PLEASE ENTER THE NUMBER OF SIDES]");
 			String color = JOptionPane.showInputDialog("And with what color pen?");
+			
+			int n = 0;
+			
+			if(nS.equals("")) {
+				n = 3;
+			}
+			else if(nS.equals("1")) {
+				n = 3;
+			}
+			else {
+				n = Integer.parseInt(nS);
+			}
 			
 			if(color.equalsIgnoreCase("red")) {
 				robo.setPenColor(Color.red);
 			}
-			while(i < 50) {
+			else if(color.equalsIgnoreCase("blue")) {
+				robo.setPenColor(Color.red);
+			}
+			else if(color.equalsIgnoreCase("orange")) {
+				robo.setPenColor(Color.orange);
+			}
+			else if(color.equalsIgnoreCase("green")) {
+				robo.setPenColor(Color.green);
+			}
+			else if(color.equalsIgnoreCase("yellow")) {
+				robo.setPenColor(Color.yellow);
+			}
+			else if(color.equalsIgnoreCase("black")) {
+				robo.setPenColor(Color.black);
+			}
+			else if(color.equalsIgnoreCase("white")) {
+				robo.setPenColor(Color.white); 
+			}
+			else if(color.equalsIgnoreCase("gray")) {
+				robo.setPenColor(Color.gray); 
+			}
+			else if(color.equalsIgnoreCase("cyan")) {
+				robo.setPenColor(Color.cyan);
+			}
+			else if(color.equalsIgnoreCase("teal")) {
+				robo.setPenColor(Color.cyan);
+			}
+			else if(color.equalsIgnoreCase("magenta")) {
+				robo.setPenColor(Color.magenta);
+			}
+			else if(color.equalsIgnoreCase("")) {
+				robo.setRandomPenColor();
+			}
+			else if(color.equalsIgnoreCase("rainbow")) {
+				robo.setPenColor(Color.red);
+				rainbow = 2;
+			}
+			else if(color.equalsIgnoreCase("crazy")) {
+				robo.setRandomPenColor();
+				crazy += 1;
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "Sorry, we don't have that color."
+						+ "[LIST OF COLORS:\n"
+						+ "RED\n"
+						+ "BLUE\n"
+						+ "ORANGE\n"
+						+ "YELLOW\n"
+						+ "GREEN\n"
+						+ "CYAN\n"
+						+ "TEAL\n"
+						+ "MAGENTA\n"
+						+ "BLACK\n"
+						+ "WHITE\n"
+						+ "AND GRAY.\n"
+						+ "ENTER NOTHING FOR A RANDOM CHOISE].");
+			}
+			int i = 0;
+			while(i < (180/n)+n) {
 				robo.move(100);
-				robo.turn(89);
-			
+				robo.turn(180-(((n-2)*180)/n)-1);
+				
+				if(crazy > 0) {
+					robo.setRandomPenColor();
+				}
+				if(rainbow == 1) {
+					robo.setPenColor(Color.red);
+				}
+				else if(rainbow == 2) {
+					robo.setPenColor(Color.orange);
+				}
+				else if(rainbow == 3) {
+					robo.setPenColor(Color.yellow);
+				}
+				else if(rainbow == 4) {
+					robo.setPenColor(Color.green);
+				}
+				else if(rainbow == 5) {
+					robo.setPenColor(Color.cyan);
+				}
+				else if(rainbow == 6) {
+					robo.setPenColor(Color.blue);
+				}
+				else if(rainbow == 7) {
+					robo.setPenColor(Color.magenta);
+				}
+				if(rainbow >= 8) {
+					rainbow = 1;
+				}
+				else if(rainbow > 0) {
+					rainbow += 1;
+				}
 				i += 1;
 			}
-	
-		
-		
-		
-		
+			i = 0;
 		}
 		//1. Create a new Robot
 	    //2. Make the robot draw a shape (this will take more than one line of code)		
