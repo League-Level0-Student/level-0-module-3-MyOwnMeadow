8a94dcbb2ddeeec1ba157d735fa979ba000c4e35
student <student@ip-172-31-56-194.us-west-2.compute.internal>
2022-02-05 16:27:41 -0800

    TRAP DOORS GALOR!

--- src/_01_else_if/_5_own_adventure/The_House.java
+++ src/_01_else_if/_5_own_adventure/The_House.java
@@ -1,6 +1,9 @@
 package _01_else_if._5_own_adventure;
 
 import javax.swing.JOptionPane;
+
+import java.util.Random;
+
 public class The_House {
 	
 	public static void main(String[]args) {
@@ -14,7 +17,7 @@ public class The_House {
 				+ "SPEAR\n"
 				+ "SPELL BOOK\n]");
 		JOptionPane.showMessageDialog(null, "Out of curiosity, you oppen the old, roting door to find\n"
-				+ "an equaly old stone hallway, with an old pully system lining the walls.");
+				+ "an equaly old stone hallway, with an antique pully system lining the walls.");
 		while(bossHP == 0) {
 			String hallDoor = hall1();
 			if(hallDoor.equalsIgnoreCase("L1")) {
@@ -77,19 +80,82 @@ public class The_House {
 		return hallDoor;
 	}
 	static int trapRoom() {
+		int i = 0;
+//Code
+		Random ran1 = new Random(2);
+		//ran1 += 10;
+		Random ran2 = new Random(2);
+		Random ran3 = new Random(2);
+		Random ran4 = new Random(2);
+//Traps
+		Random trapRow = new Random(3);
+		Random trapCol = new Random(2);
+	//
+//Other V
+		String trapDoorS = "NULL";
+		int trapDoor = 0;
+		//
+		while(i == 0) {
 		int iTrap = 0;
 		while(iTrap == 0) {
-			String trapDoor = JOptionPane.showInputDialog("There are 12 trap doors in totel.\n"
-				+ "[CHOSE A TRAP DOOR TO OPPEN]\n"
+			trapDoorS = JOptionPane.showInputDialog("There are 12 trap doors in totel.\n"
+				+ "[ENTER THE TRAP DOOR NUMBER TO OPPEN IT]\n"
 				+ "____________________\n"
-				+ "|[1a]-[2a]-[3a]-[4a]|\n"
+				+ "|[10]-[20]-[30]-[40]|\n"
 				+ "|[-[-[-[-----]-]-]-]|\n"
-				+ "|[1b]-[2b]-[3b]-[4b]|\n"
+				+ "|[11]-[21]-[31]-[41]|\n"
 				+ "|[-[-[-[-----]-]-]-]|\n"
-				+ "|[1c]-[2c]-[3c]-[4c]|\n"
+				+ "|[12]-[22]-[32]-[42]|\n"
 				+ "|___________________|");
-			iTrap += 1;
+			switch(trapDoorS) {
+			case "10":
+				iTrap += 1;
+			case "20":
+				iTrap += 1;
+			case "30":
+				iTrap += 1;
+			case "40":
+				iTrap += 1;
+			case "11":
+				iTrap += 1;
+			case "21":
+				iTrap += 1;
+			case "31":
+				iTrap += 1;
+			case "41":
+				iTrap += 1;
+			case "12":
+				iTrap += 1;
+			case "22":
+				iTrap += 1;
+			case "32":
+				iTrap += 1;
+			case "42":
+				iTrap += 1;
+			default:
+				JOptionPane.showMessageDialog(null, "Please enter a number that is on one of the trap doors.");
+			}
 		}
-		return iTrap;
+		trapDoor = Integer.parseInt(trapDoorS);
+		//if(ran1 == trapDoor);
+	}
+		return(trapDoor);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
