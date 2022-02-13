8d4c092bf522759299d08602857d99dbac78be15
student <student@ip-172-31-63-191.us-west-2.compute.internal>
2022-02-12 16:26:39 -0800

    Pickle room, part 1.2.

--- src/_01_else_if/_5_own_adventure/The_House.java
+++ src/_01_else_if/_5_own_adventure/The_House.java
@@ -5,11 +5,23 @@ import javax.swing.JOptionPane;
 import java.util.Random;
 
 public class The_House {
+//Door code
+	static Random ran = new Random();
+	static int ran1 = ran.nextInt(3);
+	static int ran2 = ran.nextInt(3);
+	static int ran3 = ran.nextInt(3);
+	static int ran4 = ran.nextInt(3);
 	
 	public static void main(String[]args) {
+	//Printing for debug
+		System.out.println("ran1");
+		System.out.println("ran2");
+		System.out.println("ran3");
+		System.out.println("ran4");
+	//=============================
 		int bossHP = 0;
-		String wep = JOptionPane.showInputDialog(null, "You are a brave advencherer. You come appon a fortus hiddin deep within the Missty Forest.\n"
-				+ "With you, you have 5 healing potions (a.k.a. Potion), 20ft of rope, a tourch, and your trusty...\n"
+		String wep = JOptionPane.showInputDialog(null, "You are a brave advencherer. Durring a walk, you stumble stumble across an old building hiddin deep within the forest.\n"
+				+ "With you, you have 5 healing potions (a.k.a. Potion), 20ft of rope, an oil lantern, and your trusty...\n"
 				+ "[CHOSE ONE:\n"
 				+ "SWORD\n"
 				+ "BOW\n"
@@ -21,17 +33,17 @@ public class The_House {
 		while(bossHP == 0) {
 			String hallDoor = hall1();
 			if(hallDoor.equalsIgnoreCase("L1")) {
-				//Trap door room (code)
+				trapRoom(); //Code
 			}
 			else if(hallDoor.equalsIgnoreCase("L2")) {
 				//Tank room (gear)
 			}
 			else if(hallDoor.equalsIgnoreCase("R1")) {
 				//Locked
-				JOptionPane.showMessageDialog(null, "It's locked. You can hear the clatering sound of pots and pans...");
+				JOptionPane.showMessageDialog(null, "It's locked. You can hear the clatering sound of pots and pans inside...");
 			}
 			else if(hallDoor.equalsIgnoreCase("R2")) {
-				//Oger room (brick)
+				//Oger room (pin and bones brick)
 			}
 			else {
 				//Final staircase
@@ -79,17 +91,11 @@ public class The_House {
 		}
 		return hallDoor;
 	}
-	static int trapRoom() {
+	static void trapRoom() {
 		int i = 0;
-//Code
-		Random ran1 = new Random(2);
-		//ran1 += 10;
-		Random ran2 = new Random(2);
-		Random ran3 = new Random(2);
-		Random ran4 = new Random(2);
 //Traps
-		Random trapRow = new Random(3);
-		Random trapCol = new Random(2);
+		int trapRow = ran.nextInt(3)+1;
+		int trapCol = ran.nextInt(4)+1;
 	//
 //Other V
 		String trapDoorS = "NULL";
@@ -99,7 +105,7 @@ public class The_House {
 		int iTrap = 0;
 		while(iTrap == 0) {
 			trapDoorS = JOptionPane.showInputDialog("There are 12 trap doors in totel.\n"
-				+ "[ENTER THE TRAP DOOR NUMBER TO OPPEN IT]\n"
+				+ "[ENTER THE TRAP DOOR NUMBER TO OPPEN IT - ENTER ''0'' TO EXIT ROOM]\n"
 				+ "____________________\n"
 				+ "|[10]-[20]-[30]-[40]|\n"
 				+ "|[-[-[-[-----]-]-]-]|\n"
@@ -108,6 +114,8 @@ public class The_House {
 				+ "|[12]-[22]-[32]-[42]|\n"
 				+ "|___________________|");
 			switch(trapDoorS) {
+			case "0":
+				iTrap += 1;
 			case "10":
 				iTrap += 1;
 			case "20":
@@ -137,9 +145,42 @@ public class The_House {
 			}
 		}
 		trapDoor = Integer.parseInt(trapDoorS);
-		//if(ran1 == trapDoor);
+		if(trapDoor == 0) {
+			JOptionPane.showMessageDialog(null, "You exit the room.");
+		}
+		else if(ran1 == trapDoor - 10) {
+			JOptionPane.showMessageDialog(null, "Climing down, you see what seems to be a recked hideout.\n"
+					+ "You spot some writing on the wall. It reads:\n"
+					+ "We have been stuck here,\n"
+					+ "For over a year.\n"
+					+ "We all live in fear,\n"
+					+ "For they are near.\n"
+					+ "You feel creeped out. You clime back up.");
+			JOptionPane.showMessageDialog(null,"While climing, you relise that the underside of the trap door had an ''X'' burned into the otherside of it.");
+		}
+		else if(ran2 == trapDoor - 20) {
+			JOptionPane.showMessageDialog(null, "Climing down, you enter what seems to be a pantry.\n"
+					+ "You spot some small writing on the wall. It reads:\n"
+					+ "Why do we have an entire room dedicated to pickles agin?\n"
+					+ "For over a year.\n"
+					+ "We all live in fear,\n"
+					+ "For they are near.\n"
+					+ "You feel creeped out. You clime back up.");
+			JOptionPane.showMessageDialog(null,"While climing, you relise that the underside of the trap door had an ''X'' burned into the otherside of it.");
+		}
+		else if(ran3 == trapDoor - 30) {
+			
+		}
+		else if(ran4 == trapDoor - 40) {
+			
+		}
+		else if(trapRow + trapCol == trapDoor) {
+			//Encounter
+		}
+		else {
+			//Empty room
+		}
 	}
-		return(trapDoor);
 	}
 }
 
