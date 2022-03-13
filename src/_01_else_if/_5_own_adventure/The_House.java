262f8cf38425197e1c188ff009845d2280ea1283
student <student@ip-172-31-62-146.us-west-2.compute.internal>
2022-03-12 16:28:47 -0800

    FINISHED TRAP ROOM!!! WO-HOO!

--- src/_01_else_if/_5_own_adventure/The_House.java
+++ src/_01_else_if/_5_own_adventure/The_House.java
@@ -12,15 +12,20 @@ public class The_House {
 	static int ran2 = ran.nextInt(3);
 	static int ran3 = ran.nextInt(3);
 	static int ran4 = ran.nextInt(3);
+
+//Colectibles
+	static String key = " don't have a key...";
+	
+//Secrits
+	static int kichin = 0;
 	
 	public static void main(String[]args) {
 	//Printing for debug
-		System.out.println("ran1");
-		System.out.println("ran2");
-		System.out.println("ran3");
-		System.out.println("ran4");
-	//Colectibles
-		String key = "don't have a key...";
+		System.out.println(ran1);
+		System.out.println(ran2);
+		System.out.println(ran3);
+		System.out.println(ran4);
+		
 	//=============================
 		
 		int bossHP = 0;
@@ -32,6 +37,14 @@ public class The_House {
 				+ "DAGGER\n"
 				+ "SPEAR\n"
 				+ "SPELL BOOK\n]");
+		
+		if(wep.equalsIgnoreCase("Skip")) {
+			trapRoom();
+		}
+		if(wep.equalsIgnoreCase("Code")) {
+			String cheater = JOptionPane.showInputDialog("[ROOM CODE IS " + ran1 + ran2 + ran3 + ran4 + "]");
+			key = "- hay, where did you get that key?";
+		}
 		JOptionPane.showMessageDialog(null, "Out of curiosity, you oppen the old, rusting door to find\n"
 				+ "an equaly old hallway, with an antique pully system lining the walls.\n"
 				+ "The Pully system seems to once have aided in the transportation of some sort of entry fee,\n"
@@ -39,7 +52,14 @@ public class The_House {
 		while(bossHP == 0) {
 			String hallDoor = hall1();
 			if(hallDoor.equalsIgnoreCase("L1")) {
-				trapRoom(); //Code
+				JOptionPane.showMessageDialog(null, "You walk into what appears to be an old dining hall,\n"
+						+ "compleat with fun decerations and a stage for preformences.");
+				JOptionPane.showMessageDialog(null, "......");
+				JOptionPane.showInputDialog(null, "Something about this room feels... off.\n"
+						+ "Like something was, and still is, hidden from view...\n"
+						+ "");
+				JOptionPane.showMessageDialog(null, "You exit the room. But, actshaly, you find some trap doors under the tables because the room with the hint does not egsist yet.");
+				trapRoom(); //Code + Key
 			}
 			else if(hallDoor.equalsIgnoreCase("L2")) {
 				//Maintnence room (gear/enconter)
@@ -64,6 +84,9 @@ public class The_House {
 		//	/(@)\
 		// <<<^>>>
 	}
+//==================================Rooms=======================================
+	
+	
 	static String hall1() {
 		String hallDoor = "null";
 		int iHall = 0;
@@ -115,9 +138,9 @@ public class The_House {
 				+ "[ENTER THE TRAP DOOR NUMBER TO OPPEN IT - ENTER ''0'' TO EXIT ROOM]\n"
 				+ "____________________\n"
 				+ "|[10]-[20]-[30]-[40]|\n"
-				+ "|[-[-[-[-----]-]-]-]|\n"
+				+ "|[-[-[-[---------]-]-]-]|\n"
 				+ "|[11]-[21]-[31]-[41]|\n"
-				+ "|[-[-[-[-----]-]-]-]|\n"
+				+ "|[-[-[-[---------]-]-]-]|\n"
 				+ "|[12]-[22]-[32]-[42]|\n"
 				+ "|___________________|");
 			switch(trapDoorS) {
@@ -148,6 +171,9 @@ public class The_House {
 			case "42":
 				iTrap += 1;
 			default:
+				//JOptionPane.showMessageDialog(null, "Please enter a number that is on one of the trap doors.");
+			}
+			if(iTrap == 0) {
 				JOptionPane.showMessageDialog(null, "Please enter a number that is on one of the trap doors.");
 			}
 		}
@@ -156,14 +182,15 @@ public class The_House {
 			JOptionPane.showMessageDialog(null, "You exit the room.");
 		}
 		else if(ran1 == trapDoor - 10) {
-			JOptionPane.showMessageDialog(null, "Climing down, you see what seems to be a recked hideout, the type that you made when you were a kid.\n"
-					+ "This one seems to be much older.\n"
+			JOptionPane.showMessageDialog(null, "Climing down, you see what seems to be a recked hideout, the type that you"
+					+ "made when you were a kid. This one seems to be much older, though.\n"
 					+ "You spot some writing on the wall, writen in child's handwriting\n"
-					+ "with some sort of driping paint(?) It reads:\n" + "\n"
-					+ "We have been stuck here,\n"
+					+ "with some sort of driping paint(?) It reads:");
+			JOptionPane.showMessageDialog(null,"We have been stuck here,\n"
 					+ "For over a year.\n"
 					+ "We all live in fear,\n"
 					+ "For they are near.");
+			JOptionPane.showMessageDialog(null, "......");
 			JOptionPane.showMessageDialog(null, "You feel creeped out. You clime back up.");
 			JOptionPane.showMessageDialog(null,"While climing, you relise that the underside of the trap door had an ''X'' burned into it.");
 		}
@@ -177,34 +204,48 @@ public class The_House {
 					+ "If we store anything else here, you know that SHE will find it. We don't want to have to lock up another room. She has already found a difrent way into the kitchen!\n"
 					+ "Why do we even have pickles at all?");
 			
-			JOptionPane.showMessageDialog(null, "...");
-			JOptionPane.showMessageDialog(null, "The pickles are already gon. Some one else ate them.\n"
-					+ "Feeling disapointed, you clime back up.");
+			JOptionPane.showMessageDialog(null, "......");
+			JOptionPane.showMessageDialog(null, "Unforchenetly, there are no more pikles left. Some one else ate them.");
+			JOptionPane.showMessageDialog(null, "Feeling disapointed, you clime back up.");
 			JOptionPane.showMessageDialog(null,"While climing, you relise that the underside of the trap door had an ''X'' burned into it.");
 		}
 		else if(ran3 == trapDoor - 30) {
-			JOptionPane.showMessageDialog(null, "You clime down the rusty lader, into the obyss below...\n"
-					+ "You find a decaying, but well-furnished room. It seems to be an ofice...? Who would want to work down Here?\n"
-					+ "A neon red stickynote caches your eye. It is writen in overly fancy, hard to read cursive. From what you can tell, it reads:\n" + "\n"
-							+ "Please change the system for generating the code for the V.I.P. stage. It only outputs 0, 1, and 2, and we don't want our custemerst to go there only to find out that some teen spray painted the [the next word is to fancy to be legible]. ");
-			JOptionPane.showMessageDialog(null, "You dout that the code has been changed recently. But you wonder where this stage could be?\n"
-					+ "The only stage you know of is back up the lader in the dining area, and that was not protected by any sort of lock. And what could be on the stage?\n"
-					+ "Feeling annoyed at the over-the-top cursive, you clime back up the lader.");
+			JOptionPane.showMessageDialog(null, "You clime down the rusty lader, into the obyss below...");
+			JOptionPane.showMessageDialog(null, "You find a decaying, but well-furnished room. It seems to be an ofice...?");
+			JOptionPane.showMessageDialog(null, "Who would want to work down Here?");
+			JOptionPane.showMessageDialog(null, "A neon red stickynote caches your eye. It is writen in overly fancy, hard to read cursive. From what you can tell, it reads:");
+			JOptionPane.showMessageDialog(null, "Please change the system for generating the code for the V.I.P. stage. It only outputs 0, 1, and 2,\n"
+					+ "and we don't want our custemerst to go there only to find out that some teen spray painted the [the next word is to fancy to be legible]. ");
+			JOptionPane.showMessageDialog(null, "You dout that the code has been changed recently, but you wonder where this stage could be?\n"
+					+ "The only stage you know of is back up the lader in the dining area, and that\n"
+					+ "was not protected by any sort of lock. And what could be on the stage?");
+			JOptionPane.showMessageDialog(null, "Feeling annoyed at the over-the-top cursive, you clime back up the lader.");
 			JOptionPane.showMessageDialog(null, "You notise a cursive ''X'' burnd into the underside of the trap door. it acors to you that maybe there is a reasen for the trapdoors being numbered the way they are...");
 		}
-		else if(ran4 == trapDoor - 40) {
-			JOptionPane.showMessageDialog(null, "You clime down the later, and enter what appers to be a stroge room for party suplies.\n"
-					+ "The only notable thing, other then the noise makers, is a key on the ground. It is labled:\n" + "\n"
-					+ "BACK UP SECURITY - USE IN ADDITION TO COMBO LOCK\n" + "\n"
-					+ "Good thing you found this key!\n"
-					+ "[KEY ADDED TO INVATORY].");
+		else if(ran4 == trapDoor - 40 || key != " have a key!") {
+			JOptionPane.showMessageDialog(null, "You clime down the later, and enter what appers to be a stroge room for party suplies.");
+			JOptionPane.showMessageDialog(null, "The only notable thing, other then the noise makers, is a key on the ground. It is labled:");
+			JOptionPane.showMessageDialog(null, "BACK UP SECURITY -\n"
+					+ "USE IN ADDITION TO COMBO LOCK");
+			JOptionPane.showMessageDialog(null, "Good thing you found this key!");
+			JOptionPane.showMessageDialog(null, "[KEY ADDED TO INVATORY].");
+			key = " have a key!";
 //ADD KEY
+		}
+		else if(ran4 == trapDoor - 40) {
+			JOptionPane.showMessageDialog(null, "You clime down the later, and enter what appers to be a stroge room for party suplies.");
+			JOptionPane.showMessageDialog(null, "......");
+			JOptionPane.showMessageDialog(null, "You've already been here. You clime back out.");
 		}
 		else if(trapRow + trapCol == trapDoor) {
 			//Encounter
 		}
-		else {
-			//Other storge room
+		else if(trapDoor != 1){
+			JOptionPane.showMessageDialog(null, "You clime down the old latter to find an old storege room,\n"
+					+ "filled to the brim with varies suplys, ranging from decaying\n"
+					+ " paper plates and utensils to varyis cleaning suplies.");
+			JOptionPane.showMessageDialog(null, ".....\n"
+					+ "There's nothing of note here. You clime back out.");
 		}
 	}
 	}
